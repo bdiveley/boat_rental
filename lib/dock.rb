e4fcca97772957f48157b92372f719088e136ad5
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-05 13:57:26 -0600

    completed rent and log hour methods

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,10 +1,27 @@
 class Dock
     attr_reader :name,
-                :max_rental_time
+                :max_rental_time,
+                :rented_list
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rented_list = []
   end
 
+  def rent(boat, renter)
+    @rented_list << [boat, renter]
+  end
+
+  def log_hour
+    @rented_list.map do |pair|
+    pair[0].add_hour
+    end
+  end
+
+  def return(boat)
+    @rented_list.find do |pair|
+      pair[0] == boat
+    end
+  end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/dock'
+require './lib/renter'
+require './lib/boat'
 require 'pry'
 
 class DockTest < Minitest::Test
@@ -17,5 +19,70 @@ class DockTest < Minitest::Test
     assert_equal "The Rowing Dock", dock.name
     assert_equal 3, dock.max_rental_time
   end
-  
+
+  def test_dock_can_rent_boat_to_renter
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    assert_equal [[kayak_1, patrick]], dock.rent(kayak_1, patrick)
+    assert_equal [[kayak_1, patrick], [kayak_2, patrick]], dock.rent(kayak_2, patrick)
+  end
+
+  def test_will_increment_up_hours_rented_for_all
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, eugene)
+
+    dock.log_hour
+
+    assert_equal 1, dock.rented_list[0][0].hours_rented
+  end
+
+  def test_will_increment_rental_hours_based_on_rental_start
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, eugene)
+    dock.log_hour
+    dock.rent(canoe, patrick)
+
+    assert_equal [2, 2, 1], dock.log_hour
+  end
+
+  def test_can_return_boat
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, eugene)
+    dock.log_hour
+    dock.rent(canoe, patrick)
+
+    assert_equal [kayak_1, patrick], dock.return(kayak_1)
+  end
+
 end
