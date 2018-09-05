d059488ea3cc935a82f10eef006cdb7a3abbedb7
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-05 13:12:47 -0600

    Complete iteration 1

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -18,4 +18,29 @@ class BoatTest < Minitest::Test
     assert_equal 20, kayak.price_per_hour
   end
 
+  def test_hours_rented_starts_at_zero
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_can_increment_up_hours_rented
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 1, kayak.add_hour
+    assert_equal 2, kayak.add_hour
+    assert_equal 3, kayak.add_hour
+  end
+
+
+    def test_stores_total_hours_rented
+      kayak = Boat.new(:kayak, 20)
+
+      kayak.add_hour
+      kayak.add_hour
+      kayak.add_hour
+
+      assert_equal 3, kayak.hours_rented
+    end
+
 end
